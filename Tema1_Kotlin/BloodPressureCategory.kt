768d7c5945a01cb1db203f1619141d265f637e5a
ionutflorina <florinaionut7@gmail.com>
2021-02-12 18:21:27 +0200

    tema1-kotlin

--- Tema1_Kotlin/BloodPressureCategory.kt
+++ Tema1_Kotlin/BloodPressureCategory.kt
@@ -10,23 +10,10 @@ enum class CategoryBloodPressure {
 }
 
 
-fun bloodPressure(systolic: Int, diastolic: Int): CategoryBloodPressure {
-    lateinit var category: CategoryBloodPressure
-    if (systolic < 120 && diastolic < 80) {
-        category = CategoryBloodPressure.NORMAL
-    } else
-        if (systolic in 120..129 && diastolic < 80) {
-            category = CategoryBloodPressure.ELEVATED
-        } else
-            if (systolic in 130..139 || diastolic in 80..89) {
-                category = CategoryBloodPressure.HYPERTENSION_STAGE1
-            } else
-                if (systolic > 180 || diastolic > 120) {
-                    category = CategoryBloodPressure.HYPERTENSIVE_CRISIS
-                } else
-                    if (systolic >= 140 || diastolic >= 90) {
-                        category = CategoryBloodPressure.HYPERTENSION_STAGE2
-                    }
-    return category
+fun bloodPressure(systolic: Int, diastolic: Int) = when {
+    systolic > 180 || diastolic > 120 -> println(CategoryBloodPressure.HYPERTENSIVE_CRISIS)
+    systolic >= 140 || diastolic >= 90 -> println(CategoryBloodPressure.HYPERTENSION_STAGE2)
+    systolic >= 130 || diastolic >= 80 -> println(CategoryBloodPressure.HYPERTENSION_STAGE1)
+    systolic >= 120 -> println(CategoryBloodPressure.ELEVATED)
+    else -> println(CategoryBloodPressure.NORMAL)
 }
-
